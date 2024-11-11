83045b331283613736ba6cb223d13ee292190a09
Muhammad Usman <bleedmenomercy786@gmail.com>
2024-11-11 21:01:11 +0000

    Added: AddressList

---
+++ ScanSpectre/AddressList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanSpectre
+{
+    public class AddressList
+    {
+        public int Start { get; }
+        public int Stop { get; }
+
+        public AddressList(int start, int stop)
+        {
+            Start = start;
+            Stop = stop;
+        }
+    }
+}
